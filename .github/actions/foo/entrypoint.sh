c264eac33b424a8e2afd72ae1a606768b6f0b20b
Preetam Jinka <Preetam@users.noreply.github.com>
2020-01-21 12:19:55 -0800

    PR branch (#1)
    * test
    
    * add status
    
    * add curl
    
    * fix flag
    
    * tweak statuses

--- .github/actions/foo/entrypoint.sh
+++ .github/actions/foo/entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
 echo "Started"
+curl -XPOST "https://api.github.com/repos/$GITHUB_REPO/statuses/$GITHUB_SHA" \
+  -H "Authorization: Bearer $GITHUB_TOKEN" \
+  -H "Content-Type: application/json" \
+  -d '{"state": "pending", "context": "Security checks", "target_url": "https://example.com"}'
 sleep 10
+curl -XPOST "https://api.github.com/repos/$GITHUB_REPO/statuses/$GITHUB_SHA" \
+  -H "Authorization: Bearer $GITHUB_TOKEN" \
+  -H "Content-Type: application/json" \
+  -d '{"state": "failure", "context": "Security checks", "target_url": "https://example.com"}'
 echo "Ended"
